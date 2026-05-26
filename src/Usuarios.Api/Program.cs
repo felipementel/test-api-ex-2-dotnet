9b086b914c6db3722a258c3462bc69933f0c0aa1
Felipe Augusto <felipementel@hotmail.com>
2026-05-26 07:14:55 -0300

    Update Program.cs

--- src/Usuarios.Api/Program.cs
+++ src/Usuarios.Api/Program.cs
@@ -2,6 +2,7 @@ using Usuarios.Adapters.Repositories;
 using Usuarios.Api.Endpoints;
 using Usuarios.Application.Services;
 using Usuarios.Domain.Ports;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,15 @@ builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
 builder.Services.AddSingleton<UserService>();
 
 var app = builder.Build();
+
+var isOpenApiGeneration =
+    Assembly.GetEntryAssembly()?.GetName().Name == "GetDocument.Insider";
+
+if (!isOpenApiGeneration)
+{
+    // Coisas que não devem rodar durante a geração do OpenAPI
+    // Ex: conexões externas, validações fortes de secrets, hosted services etc.
+}
 
 app.UseSwagger();
 app.UseSwaggerUI();
